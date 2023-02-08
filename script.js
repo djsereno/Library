396a050de7b7a40b0e45c9ab80011ba55069f0f3
djsereno <djsereno91@gmail.com>
2023-02-07 23:12:33 -0800

    Add new book form

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const libraryDiv = document.querySelector(".library");
+const libraryDiv = document.querySelector('.library');
+const newBookButton = document.querySelector('.new-book');
+const newBookForm = document.querySelector('.new-book-form');
+const addBookButton = document.querySelector('.add-book');
 let myLibrary = [];
+
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+let harryPotter = new Book('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 341, true);
+let romeoAndJuliet = new Book('Romeo and Juliet', 'Shakespeare', 201, false);
+addBookToLibrary(theHobbit);
+addBookToLibrary(harryPotter);
+addBookToLibrary(romeoAndJuliet);
+updateLibrary();
+
+console.log(theHobbit.info());
+console.log(harryPotter.info());
+console.log(romeoAndJuliet.info());
+
+newBookButton.addEventListener('click', (e) => {
+  console.log(e.currentTarget);
+});
+addBookButton.addEventListener('click', (e) => {
+  console.log(e.currentTarget);
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -17,23 +39,11 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-let harryPotter = new Book('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 341, true);
-let romeoAndJuliet = new Book('Romeo and Juliet', 'Shakespeare', 201, false);
-
-console.log(theHobbit.info());
-console.log(myLibrary);
-addBookToLibrary(theHobbit);
-addBookToLibrary(harryPotter);
-addBookToLibrary(romeoAndJuliet);
-
 function updateLibrary() {
-  myLibrary.forEach(book => {
-    const tag = document.createElement("li");
-    const text = document.createTextNode(book.title);
+  myLibrary.forEach((book) => {
+    const tag = document.createElement('li');
+    const text = document.createTextNode(book.info());
     tag.appendChild(text);
     libraryDiv.appendChild(tag);
-  })
+  });
 }
-
-updateLibrary();
