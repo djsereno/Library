8094433a7926a8c56a6809b78e95d83016d732ff
djsereno <djsereno91@gmail.com>
2023-02-07 23:42:10 -0800

    Update addBookToLibrary to read form data

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 const libraryDiv = document.querySelector('.library');
 const newBookButton = document.querySelector('.new-book');
 const newBookForm = document.querySelector('.new-book-form');
+const formInputs = document.querySelectorAll('.new-book-form input');
 const addBookButton = document.querySelector('.add-book');
 let myLibrary = [];
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
 let harryPotter = new Book('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 341, true);
 let romeoAndJuliet = new Book('Romeo and Juliet', 'Shakespeare', 201, false);
-addBookToLibrary(theHobbit);
-addBookToLibrary(harryPotter);
-addBookToLibrary(romeoAndJuliet);
+addBookToLibrary(false, theHobbit);
+addBookToLibrary(false, harryPotter);
+addBookToLibrary(false, romeoAndJuliet);
 updateLibrary();
 
 console.log(theHobbit.info());
 console.log(harryPotter.info());
 console.log(romeoAndJuliet.info());
 
-newBookButton.addEventListener('click', (e) => {
-  console.log(e.currentTarget);
-});
-addBookButton.addEventListener('click', (e) => {
-  console.log(e.currentTarget);
-});
+// newBookButton.addEventListener('click', displayForm);
+addBookButton.addEventListener('click', addBookToLibrary);
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.isRead = isRead;
   this.info = () => {
     let string = this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ';
-    this.read ? (string += 'read') : (string += 'not read yet');
+    this.isRead ? (string += 'read') : (string += 'not read yet');
     return string;
   };
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary(event, book) {
+  if (book) {
+    myLibrary.push(book);
+  } else {
+    formInputs.forEach((entry) => {
+      entry.type === 'checkbox'
+        ? console.log(entry.name, entry.checked)
+        : console.log(entry.name, entry.value);
+    });
+  }
 }
 
 function updateLibrary() {
